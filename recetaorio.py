b38ed32b409f3373a20310ad3e45098500762afd
Cuauhtli <cuau_daali@hotmail.com>
2023-01-30 15:50:32 -0600

    Option 1, 4, and 5 complete

--- recetaorio.py
+++ recetaorio.py
@@ -1,4 +1,4 @@
-from os import system, name, listdir
+from os import system, name, listdir, remove, rmdir
 from pathlib import Path
 
 
@@ -25,37 +25,95 @@ def recetario():
                 print("La opción 3 le va a preguntar el nombre de la categoría que quiere crear y va a generar una carpeta nueva con ese nombre.")
             case "4":
                 print("La opción 4, hará todo lo mismo que la opción uno, pero en vez de leer la receta, la va a eliminar")
+                option4()
             case "5":
                 print("La opción 5, le va a preguntar qué categoría quiere eliminar")
                 option5()
             case "6":
-                print("Usted eligio salir del recetario")
+                clear()
                 valid = True
+                print("Usted eligio salir del recetario")
+                
             case _:
                 print("No existe esta opcion")
 
 
 def option1():
-    clear()
-    input(f"Elija una categoria ")
-    recetario()
+    ruta = Path(Path.cwd(), 'Recetas')
+    listCategory =  sorted(listdir(ruta))
+    stringCategory = ", ".join(listCategory)
+    validCategory = False
+    while not validCategory:
+        clear()
+        category = input(f"Escriba la categoria donde se encuentra la receta que desea leer: {stringCategory}: ")
+        if category.lower() in [x.lower() for x in listCategory]:
+            rutaReceta = Path(ruta, category.lower().capitalize())
+            listRecetas = sorted(listdir(rutaReceta))
+            listWhitoutTxt = [x.replace(".txt", "") for x in listRecetas]
+            stringRecetas = ", ".join(listWhitoutTxt)
+            validCategory = True
+            validReceta = False
+            while not validReceta:
+                receta = input(f"Que receta desea leer: {stringRecetas}: ")
+                if receta.lower() in [x.lower() for x in listWhitoutTxt]:
+                    indexReceta = [x.lower() for x in listWhitoutTxt].index(receta.lower())
+                    fileReceta = open(Path(rutaReceta, listRecetas[indexReceta]))
+                    print(fileReceta.read())
+                    input("Desea regresar")
+                    validReceta = True
+                else:
+                    input("La receta que escogio no se encuentra de enter para escoger nuevamente")
+        else:
+            input("La categoria que escogio no se encuentra de enter para escoger nuevamente")
 
 def option2():
     clear()
-    recetario()
 
 def option3():
     clear()
-    recetario()
 
 def option4():
-    clear()
-    recetario()
+    ruta = Path(Path.cwd(), 'Recetas')
+    listCategory =  sorted(listdir(ruta))
+    stringCategory = ", ".join(listCategory)
+    validCategory = False
+    while not validCategory:
+        clear()
+        category = input(f"Escriba la categoria donde se encuentra la receta que desea eliminar {stringCategory}: ")
+        if category.lower() in [x.lower() for x in listCategory]:
+            rutaReceta = Path(ruta, category.lower().capitalize())
+            listRecetas = sorted(listdir(rutaReceta))
+            listWhitoutTxt = [x.replace(".txt", "") for x in listRecetas]
+            stringRecetas = ", ".join(listWhitoutTxt)
+            validCategory = True
+            validReceta = False
+            while not validReceta:
+                receta = input(f"Que receta desea eliminar {stringRecetas}: ")
+                if receta.lower() in [x.lower() for x in listWhitoutTxt]:
+                    indexReceta = [x.lower() for x in listWhitoutTxt].index(receta.lower())
+                    input(f"Se eliminara la receta {listWhitoutTxt[indexReceta]}")
+                    remove(Path(rutaReceta, listRecetas[indexReceta]))
+                    validReceta = True
+                else:
+                    input("La receta que escogio no se encuentra de enter para escoger nuevamente")
+        else:
+            input("La categoria que escogio no se encuentra de enter para escoger nuevamente")
 
-def option5():
-    clear()
-    input(f"Escriba la categoria que desea eliminar {listdir(Path.cwd())}")
-    recetario()
+def option5():    
+    ruta = Path(Path.cwd(), 'Recetas')
+    listCategory =  sorted(listdir(ruta))
+    stringCategory = ", ".join(listCategory)
+    validCategory = False
+    while not validCategory:
+        clear()
+        category = input(f"Escriba la categoria que desea eliminar {stringCategory}: ")
+        if category.lower() in [x.lower() for x in listCategory]:
+            indexCategory = [x.lower() for x in listCategory].index(category.lower())
+            print("Borrar directorio")
+            rmdir(Path(ruta, listCategory[indexCategory]))
+            validCategory = True
+        else:
+            input("La categoria que escogio no se encuentra de enter para escoger nuevamente")
 
 
 def clear():
@@ -64,6 +122,6 @@ def clear():
     else:
         system('clear')
 clear()
-print(f" Bienvenido las recetas se encuentran en {Path.cwd()}")
+print(f" Bienvenido las recetas se encuentran en {Path(Path.cwd(), 'Recetas')}")
 input("De un enter para continuar")
 recetario()
